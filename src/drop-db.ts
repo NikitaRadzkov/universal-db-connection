bf598496d5c67fc4e043ad389acf9ed972ac7566
Nikita Radzkou <nikita.radzkou@gmail.com>
2023-02-12 01:41:56 +0300

    Add drop database script

---
+++ src/drop-db.ts
@@ -0,0 +1,22 @@
+import { DataSource } from "typeorm"
+import { MariaDBDataSource, MSSQLDataSource, MySQLDataSource, PostgresSQLDataSource, SQLiteDataSource } from "./data-source"
+
+const dropDatabase = async (db: DataSource) => {
+  try {
+      await db.initialize()
+      await db.dropDatabase()
+      await db.destroy()
+  } catch (err) {
+      console.error(err)
+  }
+}
+
+const dropAllDatabases = async () => {
+  await dropDatabase(PostgresSQLDataSource)
+  await dropDatabase(MySQLDataSource)
+  await dropDatabase(MariaDBDataSource)
+  await dropDatabase(SQLiteDataSource)
+  await dropDatabase(MSSQLDataSource)
+}
+
+dropAllDatabases()
